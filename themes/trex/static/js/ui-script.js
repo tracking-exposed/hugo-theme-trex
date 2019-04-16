ff162010343a3b2db7eec901d1c9a713f070e17c
lrnzctld <lrnzctld@gmail.com>
2019-04-16 10:09:33 +0200

    add static elements

---
+++ themes/trex/static/js/ui-script.js
@@ -0,0 +1,158 @@
+/**
+ * navigation menu
+ */
+( function() {
+    var container, button, menu, links, i, len, masthead;
+
+    container = document.getElementById( 'site-navigation' );
+    if ( ! container ) {
+        return;
+    }
+    masthead = document.getElementById('masthead');
+
+    button = container.getElementsByTagName( 'button' )[0];
+    if ( 'undefined' === typeof button ) {
+        return;
+    }
+
+    menu = container.getElementsByTagName( 'ul' )[0];
+
+    // Hide menu toggle button if menu is empty and return early.
+    if ( 'undefined' === typeof menu ) {
+        button.style.display = 'none';
+        return;
+    }
+
+    menu.setAttribute( 'aria-expanded', 'false' );
+    if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
+        menu.className += ' nav-menu';
+    }
+
+    button.onclick = function() {
+        if ( -1 !== container.className.indexOf( 'toggled' ) ) {
+            container.className = container.className.replace( ' toggled', '' );
+            masthead.className = masthead.className.replace( ' mobile', '' );
+            button.setAttribute( 'aria-expanded', 'false' );
+            menu.setAttribute( 'aria-expanded', 'false' );
+        } else {
+            container.className += ' toggled';
+            masthead.className += ' mobile';
+            button.setAttribute( 'aria-expanded', 'true' );
+            menu.setAttribute( 'aria-expanded', 'true' );
+        }
+    };
+
+    // Get all the link elements within the menu.
+    links    = menu.getElementsByTagName( 'a' );
+
+    // Each time a menu link is focused or blurred, toggle focus.
+    for ( i = 0, len = links.length; i < len; i++ ) {
+        links[i].addEventListener( 'focus', toggleFocus, true );
+        links[i].addEventListener( 'blur', toggleFocus, true );
+    }
+
+    /**
+     * Sets or removes .focus class on an element.
+     */
+    function toggleFocus() {
+        var self = this;
+
+        // Move up through the ancestors of the current link until we hit .nav-menu.
+        while ( -1 === self.className.indexOf( 'nav-menu' ) ) {
+
+            // On li elements toggle the class .focus.
+            if ( 'li' === self.tagName.toLowerCase() ) {
+                if ( -1 !== self.className.indexOf( 'focus' ) ) {
+                    self.className = self.className.replace( ' focus', '' );
+                } else {
+                    self.className += ' focus';
+                }
+            }
+
+            self = self.parentElement;
+        }
+    }
+
+    ( function( container ) {
+        var touchStartFn, i,
+            parentLink = container.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' );
+
+        if ( 'ontouchstart' in window ) {
+            touchStartFn = function( e ) {
+                var menuItem = this.parentNode, i;
+
+                if ( ! menuItem.classList.contains( 'focus' ) ) {
+                    e.preventDefault();
+                    for ( i = 0; i < menuItem.parentNode.children.length; ++i ) {
+                        if ( menuItem === menuItem.parentNode.children[i] ) {
+                            continue;
+                        }
+                        menuItem.parentNode.children[i].classList.remove( 'focus' );
+                    }
+                    menuItem.classList.add( 'focus' );
+                } else {
+                    menuItem.classList.remove( 'focus' );
+                }
+            };
+
+            for ( i = 0; i < parentLink.length; ++i ) {
+                parentLink[i].addEventListener( 'touchstart', touchStartFn, false );
+            }
+        }
+    }( container ) );
+} )();
+
+
+jQuery(document).ready(function($) {
+    /**
+     * overlay function
+     */
+    /* general overlay */
+    function openOverlay (id) {
+        var overlay = $(id);
+        var shade = $('#overlay_shade');
+
+        shade.fadeTo(200, 0.8, function() {
+            var props = {
+                overlayWidth    : overlay.width(),
+                scrTop          : $(window).scrollTop(),
+                viewPortWidth   : $(window).width()
+            };
+            var leftPos = ((props.viewPortWidth - props.overlayWidth) / 2) - 30;
+            overlay.css({
+                display : 'block',
+                opacity : 0,
+                top : '-=9999',
+                left : leftPos+'px'
+            }).animate({
+                top : props.scrTop + 80,
+                opacity : 1
+            }, 800, function(){
+                shade.addClass('hideable');
+                overlay.addClass('hideable');
+            });
+        });
+    }
+    /*close overlay*/
+    function closeOverlay() {
+        var overlay= $('.overlay');
+        var shade = $('#overlay_shade');
+        //verifica che l'animazione si completa
+        if (shade.hasClass('hideable') && overlay.hasClass('hideable')) {
+            overlay.animate({
+                top: '-=9999',
+                opacity: 0
+            }, 200, function () {
+                shade.fadeOut(200);
+                $(this).css('display', 'none');
+            });
+            overlay.removeClass('hideable');
+            shade.removeClass('hideable');
+        }
+    }
+    /*trigger close*/
+    $('#overlay_shade, .overlay a.close-btn').on('click', function(e) {
+        e.preventDefault();
+        closeOverlay();
+    });
+});
